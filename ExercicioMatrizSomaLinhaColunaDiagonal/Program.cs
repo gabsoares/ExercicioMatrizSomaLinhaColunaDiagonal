f554a43844d6bac47d20900b0adb57a0ad16a355
gabsoares <soaresgabriel68@gmail.com>
2024-04-25 11:44:21 -0300

    Feature - implementação de função para facilitar a leitura do código

--- ExercicioMatrizSomaLinhaColunaDiagonal/Program.cs
+++ ExercicioMatrizSomaLinhaColunaDiagonal/Program.cs
@@ -1,84 +1,145 @@
-﻿int qtdLinhas = 4, qtdColunas = 4;
+﻿int qtdLinhas = 3, qtdColunas = 3, inicio = 0, fim = 0;
 int[,] matriz = new int[qtdLinhas, qtdColunas];
 int[] somaLinha = new int[6];
 int[] somaColuna = new int[6];
 int somaDiagonal = 0, somaDiagonal2 = 0;
 
-for (int linha = 0; linha < qtdLinhas; linha++)
+int[,] CriarMatriz(int[,] matrizParam, int i, int f)
 {
-    for (int coluna = 0; coluna < qtdColunas; coluna++)
+    Console.Write("A partir de qual número a matriz deve ser gerada: ");
+    inicio = int.Parse(Console.ReadLine());
+    Console.Write("Até qual número a matriz deve ser gerada: ");
+    fim = int.Parse(Console.ReadLine());
+    for (int linha = 0; linha < qtdLinhas; linha++)
     {
-        matriz[linha, coluna] = new Random().Next(1, 10);
-    }
-}
-for (int linha = 0; linha < qtdLinhas; linha++)
-{
-    Console.WriteLine();
-    for (int coluna = 0; coluna < qtdColunas; coluna++)
-    {
-        Console.Write(matriz[linha, coluna] + " ");
-    }
-}
-Console.WriteLine();
-
-//Soma de linha
-for (int linha = 0; linha < qtdLinhas; linha++)
-{
-    for (int coluna = 0; coluna < qtdColunas; coluna++)
-    {
-        somaLinha[linha] += matriz[linha, coluna];
-    }
-}
-//Soma de coluna
-for (int linha = 0; linha < qtdLinhas; linha++)
-{
-    for (int coluna = 0; coluna < qtdColunas; coluna++)
-    {
-        somaColuna[coluna] += matriz[linha, coluna];
-    }
-}
-
-//Soma diagonal principal
-for (int linha = 0; linha < qtdLinhas; linha++)
-{
-    for (int coluna = 0; coluna < qtdColunas; coluna++)
-    {
-        if (linha == coluna)
+        for (int coluna = 0; coluna < qtdColunas; coluna++)
         {
-            somaDiagonal += matriz[linha, coluna];
+            matrizParam[linha, coluna] = new Random().Next(inicio, fim);
+        }
+    }
+    inicio = i;
+    fim = f;
+    return matrizParam;
+}
+
+void ImprimirMatriz(int[,] matrizParam, string mensagem)
+{
+    Console.WriteLine(mensagem);
+    for (int linha = 0; linha < qtdLinhas; linha++)
+    {
+        Console.WriteLine();
+        for (int coluna = 0; coluna < qtdColunas; coluna++)
+        {
+            Console.Write(matrizParam[linha, coluna] + " ");
         }
     }
 }
 
-//Soma diagonal secundária
-for (int linha = 0; linha < qtdLinhas; linha++)
+int Menu()
 {
-    for (int coluna = 0; coluna < qtdColunas; coluna++)
+    Console.WriteLine("ESCOLHA UMA DAS OPÇÕES ABAIXO");
+    Console.WriteLine("1- ADIÇÃO DAS LINHAS DA MATRIZ");
+    Console.WriteLine("2- ADIÇÃO DAS COLUNAS DA MATRIZ");
+    Console.WriteLine("3- ADIÇÃO DA DIAGONAL PRINCIPAL DA MATRIZ");
+    Console.WriteLine("4- ADIÇÃO DA DIAGONAL SECUNDARIA DA MATRIZ");
+    Console.Write("OPCAO ESCOLHIDA: ");
+    int opcao = int.Parse(Console.ReadLine());
+
+    return opcao;
+}
+
+void SomarLinhaMatriz(int[,] matrizParam)
+{
+    for (int linha = 0; linha < qtdLinhas; linha++)
     {
-        if (linha != coluna && linha + coluna == qtdLinhas - 1)
+        for (int coluna = 0; coluna < qtdColunas; coluna++)
         {
-            somaDiagonal2 += matriz[linha, coluna];
+            somaLinha[linha] += matrizParam[linha, coluna];
         }
+    }
+
+    for (int linha = 0; linha < qtdLinhas; linha++)
+    {
+        Console.Write($"Soma da {linha + 1}° linha: {somaLinha[linha]}\n");
     }
 }
 
-Console.WriteLine();
-
-for (int linha = 0; linha < qtdLinhas; linha++)
+void SomarColunaMatriz(int[,] matrizParam)
 {
-    Console.Write($"Soma da {linha + 1}° linha: {somaLinha[linha]}\n");
+    for (int linha = 0; linha < qtdLinhas; linha++)
+    {
+        for (int coluna = 0; coluna < qtdColunas; coluna++)
+        {
+            somaColuna[coluna] += matrizParam[linha, coluna];
+        }
+    }
+
+    for (int coluna = 0; coluna < qtdLinhas; coluna++)
+    {
+        Console.Write($"Soma da {coluna + 1}° coluna: {somaColuna[coluna]}\n");
+    }
 }
 
-Console.WriteLine();
-
-for (int coluna = 0; coluna < qtdLinhas; coluna++)
+void SomarDiagonalPrincipalMatriz(int[,] matrizParam)
 {
-    Console.Write($"Soma da {coluna + 1}° coluna: {somaColuna[coluna]}\n");
+    for (int linha = 0; linha < qtdLinhas; linha++)
+    {
+        for (int coluna = 0; coluna < qtdColunas; coluna++)
+        {
+            if (linha == coluna)
+            {
+                somaDiagonal += matrizParam[linha, coluna];
+            }
+        }
+    }
+
+    Console.Write($"Soma da diagonal principal: {somaDiagonal}");
 }
 
-Console.WriteLine();
-Console.Write($"Soma da diagonal principal: {somaDiagonal}");
+void SomarDiagonalSecundariaMatriz(int[,] matrizParam)
+{
+    for (int linha = 0; linha < qtdLinhas; linha++)
+    {
+        for (int coluna = 0; coluna < qtdColunas; coluna++)
+        {
+            if (linha != coluna && linha + coluna == qtdLinhas - 1)
+            {
+                somaDiagonal2 += matriz[linha, coluna];
+            }
+        }
+    }
 
+    Console.Write($"Soma da diagonal secundária: {somaDiagonal2}");
+}
+
+CriarMatriz(matriz, inicio, fim);
+ImprimirMatriz(matriz, "\nMatriz para operações");
+Console.WriteLine();
 Console.WriteLine();
 
-Console.Write($"Soma da diagonal secundária: {somaDiagonal2}");
+int opcao = 0;
+do
+{
+    opcao = Menu();
+    switch (opcao)
+    {
+        case 1:
+            SomarLinhaMatriz(matriz);
+            break;
+
+        case 2:
+            SomarColunaMatriz(matriz);
+            break;
+
+        case 3:
+            SomarDiagonalPrincipalMatriz(matriz);
+            break;
+
+        case 4:
+            SomarDiagonalSecundariaMatriz(matriz);
+            break;
+        default:
+            Console.WriteLine("\nÉ apenas um dos números sugeridos, tente novamente\n");
+            break;
+    }
+} while ((opcao < 1) || (opcao > 4));
